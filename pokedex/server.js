0c36ae8564ea25607cb4955a9a500974a916c23a
Trevor Tomesh <slugzzz@gmail.com>
2025-11-19 13:00:15 -0600

    push

--- pokedex/server.js
+++ pokedex/server.js
@@ -1,10 +1,10 @@
 const express = require("express");
-const cors = require("cors");
+//const cors = require("cors");
 const fs = require("fs");
 const path = require("path");
 
 const app = express();
-app.use(cors());
+//app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
 
